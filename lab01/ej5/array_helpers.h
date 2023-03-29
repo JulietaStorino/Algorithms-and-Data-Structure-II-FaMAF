d88dbdafd1d4724ac58d97a0bf0fa97eeca08127
julieta_storino <julieta_storino@mi.unc.edu.ar>
2023-03-29 19:24:32 -0300

    Laboratorio corregido y entregado

--- lab01/ej5/array_helpers.h
+++ lab01/ej5/array_helpers.h
@@ -1,3 +1,6 @@
+#ifndef ARRAY_HELPER_H
+#define ARRAY_HELPER_H
+
 unsigned int array_from_file(int array[],
                              unsigned int max_size,
                              const char *filepath); 
@@ -5,3 +8,5 @@ unsigned int array_from_file(int array[],
 void array_dump(int a[], unsigned int length);
 
 mybool array_is_sorted(int a[], unsigned int length);
+
+#endif
